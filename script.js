136fa765db07a22e9336d08eef024c397bc14bc7
aaishikasb <mail.aaishika@gmail.com>
2020-09-18 15:23:13 +0530

    Copy Function

---
+++ script.js
@@ -0,0 +1,9 @@
+// Copy Function
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+  
