be1701b3e36f2010b1c373d2f26f6cde699c39ea
aaishikasb <mail.aaishika@gmail.com>
2020-09-18 15:39:53 +0530

    Add Emoji Dataset

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function copyToClipboard(element) {
     $temp.remove();
   }
   
+// Generator
+function generator() {
+    // List
+    var firstPart = ["The Coldness of", "The Journey to", "Altered by", "A Parallel of", "The Return of"];
+    var name = firstPart[Math.floor(Math.random() * firstPart.length)];
+    //alert(name);
+    // Remove ex
+    if (document.getElementById("name")) {
+    document.getElementById("placeholder").removeChild(document.getElementById("name"));
+    }
+    // Add new
+    var element = document.createElement("div");
+    element.setAttribute("id", "name");
+    element.appendChild(document.createTextNode(name));
+    document.getElementById("placeholder").appendChild(element);
+    }
