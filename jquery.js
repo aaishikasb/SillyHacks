97a184dc9470ade267044057f70c88bd90dc092f
aaishikasb <mail.aaishika@gmail.com>
2020-09-19 10:32:40 +0530

    Added Popover

--- jquery.js
+++ jquery.js
@@ -1,11 +1,11 @@
 // Popover
 
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
+
 $('#example').popover('hide');
 $('#example').click(function() {
 
   $('#example').popover('show');
 });
-
-$(function () {
-    $('[data-toggle="popover"]').popover()
-})
